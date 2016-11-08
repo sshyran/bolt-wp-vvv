33fb6718e2734f4b30a28ff228af0e6cb55796b2
Justin Kopepasah <justin@kopepasah.com>
2016-11-08 14:28:58 +0900

    Update the deploy.sh messages.

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,20 @@ cd $TRAVIS_BUILD_DIR
 # --- Safety checks
 # If a Pantheon site is not set, bail.
 if [ -z "$PANTHEON_SITE" ]; then
-	echo "Skipping deploy because no Pantheon site is set."
+	echo "===> Skipping deploy because PANTHEON_SITE environment variable is not set."
 	exit 0
 fi
 
 # If this is a check on a pull request, bail.
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
-	echo "Skipping deploy because this is a pull request."
+	echo "===> Skipping deploy because this is a pull request."
 	exit 0
 fi
 
 # If this check is not on master or develop branch, bail.
 if [ $TRAVIS_BRANCH != "master" && $TRAVIS_BRANCH != "develop" ]; then
-	echo "Skipping because you are on ${TRAVIS_BRANCH}."
-	echo "Deploys only happen on master and develop branches."
+	echo "===> Skipping because you are on ${TRAVIS_BRANCH}."
+	echo "===> Deploys only happen on master and develop branches."
 	exit 0
 fi
 
@@ -39,7 +39,7 @@ STATUS=$(git status --porcelain)
 
 # If there are no changes, bail... otherwise add, commit and push.
 if [ -z "$STATUS" ]; then
-	echo "Skipping because there are no changes to deploy to the site."
+	echo "===> Skipping deploy because there are no changes to commit."
 	exit 0
 else
 	git config user.name $NAME
